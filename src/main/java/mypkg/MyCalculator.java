d97997912a2243a903a7a4c219e2730b3bdbb1b5
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-07-29 15:53:25 +0530

    final commit

---
+++ src/main/java/mypkg/MyCalculator.java
@@ -0,0 +1,21 @@
+package mypkg;
+
+public class MyCalculator {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		
+		MyCalculator ob = new MyCalculator();
+		System.out.println("Sum is: "+ob.sum(20, 10));
+		System.out.println("Sum is: "+ob.sub(20, 10));	
+	}
+}
